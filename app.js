a4acdd8791516b5d6028d220d6bcf469824e995a
Junxiong.Xu <Junxiong.Xu@geely.com>
2019-03-29 17:03:10 +0800

    '中间件参数变更'

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: '10000kb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
